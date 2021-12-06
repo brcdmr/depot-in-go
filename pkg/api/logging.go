845696a3b6385ceede2d7830ce742932d2fc449f
brcdmr <bsdemirel@gmail.com>
2021-12-06 22:12:50 +0300

    readme

--- pkg/api/logging.go
+++ pkg/api/logging.go
@@ -16,3 +16,24 @@ func LoggingMiddleware(target http.Handler) http.Handler {
 	})
 
 }
+
+// func LoggingMiddlewareWtRecover(logger log.Logger) func(http.Handler) http.Handler {
+// 	return func(next http.Handler) http.Handler {
+// 		fn := func(w http.ResponseWriter, r *http.Request) {
+// 			defer func() {
+// 				if err := recover(); err != nil {
+// 					w.WriteHeader(http.StatusInternalServerError)
+
+// 					log.Printf("Recoverd Error: %s ", err)
+// 				}
+// 			}()
+
+// 			startTimer := time.Now()
+// 			next.ServeHTTP(w, r)
+// 			log.Printf("url: %s - method: %s\t remoteIP: %s\t perf: %v", r.RequestURI, r.Method, r.RemoteAddr, time.Since(startTimer))
+
+// 		}
+
+// 		return http.HandlerFunc(fn)
+// 	}
+// }
